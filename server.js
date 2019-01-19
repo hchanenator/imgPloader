a737029499c58dc270c1af5c1431f9b78f21481d
herb.chan <herb.chan@4finance.com>
2019-01-18 20:07:48 -0500

    Update packages.  Add entry point and configuration

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+config = require('./config/configure');
+const app = express();
+
+app.set('port', process.env.PORT || 3300);
+app.set('views', `${__dirname}/views`);
+app = config(app);
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello world!');
+// });
+
+app.use('/get_data', (req, res) => {
+  console.log('Hello world!');
+  return next();
+}, (err, req, res, next) => {
+  console.log('Hello world, again!');
+  return next();
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Server up: http://localhost:${app.get('port')}`);
+});
