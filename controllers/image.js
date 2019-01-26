3cb6833f1e29c6418fa4836c7ffaabb0e8e65c0c
HChan <hchanenator@gmail.com>
2019-01-26 16:44:08 -0500

    Add fake data models for testing

--- controllers/image.js
+++ controllers/image.js
@@ -1,6 +1,32 @@
 module.exports = {
   index: (req, res) => {
-    res.render('images');
+    const ViewModel = {
+      image: {
+        uniqueId: 1,
+        title: 'Sample Image 1',
+        description: 'This is a sample.',
+        filename: 'sample1.jpg',
+        Views: 0,
+        likes: 0,
+        timestamp: Date.now()
+      },
+      comments: [{
+        image_id: 1,
+        email: 'test@testing.com',
+        name: 'Test Tester',
+        gravatar: 'http://lorempixel.com/75/75/animals/1',
+        comment: 'This is a test comment...',
+        timestamp: Date.now()
+      }, {
+        image_id: 1,
+        email: 'test@testing.com',
+        name: 'Test Tester',
+        gravatar: 'http://lorempixel.com/75/75/animals/2',
+        comment: 'Another followup comment!',
+        timestamp: Date.now()
+      }]
+    };
+    res.render('images', ViewModel);
   },
   create(req, res) {
     res.send('The image:create POST controller');
